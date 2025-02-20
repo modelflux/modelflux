a6a98957cb67595d85b373eb1e72501eb1afc2ed
Weike Qu <47606630+weikequ@users.noreply.github.com>
2025-02-20 11:23:18 -0800

    feat: basic actions support (#9)
    Changes made:
    - Adds Model Actions
    - Adds ROADMAP.md

---
+++ ROADMAP.md
@@ -0,0 +1,52 @@
+# Project Roadmap
+
+## CLI Commands
+
+- [ ] pull
+- [ ] run
+- [ ] list, ls
+- [ ] validate
+- [ ] push (future)
+- [ ] explore (future)
+
+## Service (future)
+
+- [ ] Abstract CLI backend to be REST/GRPC compliant
+
+## SDK (future)
+
+- [ ] Add SDK for popular languages using the service to allow programmatic integrations
+
+## Server
+
+- [ ] Landing page
+- [ ] Workflow hub
+- [ ] Tools hub
+- [ ] Model actions hub (future)
+- [ ] User accounts (future)
+- [ ] Run workflows on our servers (future)
+
+## Nodes (each with inputs/outputs)
+
+- [ ] Base models
+  - [ ] Azure
+  - [ ] OpenAI
+  - [ ] Local (Ollama serve)
+- [ ] Memory store ?
+- [ ] Model actions
+  - [ ] Summary
+  - [ ] Decision
+  - [ ] Categorization
+  - [ ] Select tool to use
+  - [ ] Custom
+  - [ ] Monitor (agent)
+- [ ] Tools
+- [ ] Triggers
+  - [ ] Time based
+  - [ ] API based?
+- [ ] Conditionals
+- [ ] Loops
+
+## GUI
+
+- [ ] React flow based node editing
---
+++ pkg/action/action.go
@@ -0,0 +1,70 @@
+package action
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/modelflux/modelflux/pkg/model"
+)
+
+type Action[ActionInput string | []string] interface {
+	Run(input ActionInput, m model.Model) (ActionInput, error)
+}
+
+const LOOP_LIMIT = 100
+
+type CustomAction struct {
+	Prompt string
+}
+
+func (a *CustomAction) Run(input string, m model.Model) (string, error) {
+	systemPropmt := "SYSTEM: " + a.Prompt + "\n"
+	userPrompt := "USER: " + input + "\n"
+	return m.Generate(systemPropmt + userPrompt)
+}
+
+type SummarizeAction struct {
+	WordCount int64
+}
+
+func (a *SummarizeAction) Run(input string, m model.Model) (string, error) {
+	systemPrompt := "SYSTEM: Summarize the following document concisely," +
+		" capturing only the most essential points, key takeaways," +
+		" and critical insights. Exclude unnecessary details, examples," +
+		" and repetitions while maintaining clarity and completeness. Keep the summary under " +
+		strconv.Itoa(int(a.WordCount)) + " words."
+	return m.Generate(systemPrompt + "\n" + input)
+}
+
+type CategorizeAction struct {
+	Categories []string
+}
+
+func (a *CategorizeAction) Run(input []string, m model.Model) ([]string, error) {
+	systemPrompt := "SYSTEM: You help classify a list of items into different types." +
+		" You will respond with the category ONLY and NOTHING else. Here is a list of" +
+		" categories to classify: " + strings.Join(a.Categories, ", ")
+
+	categorizedItems := make([]string, len(input))
+
+	for i, item := range input {
+		userPrompt := "USER: " + item + "\n"
+		for j := range LOOP_LIMIT {
+			category, err := m.Generate(systemPrompt + "\n" + userPrompt)
+			if err != nil {
+				return nil, err
+			}
+			if slices.Contains(a.Categories, category) {
+				categorizedItems[i] = category
+				break
+			}
+			if j == LOOP_LIMIT-1 {
+				return nil, fmt.Errorf("Loop limit exceeded")
+			}
+		}
+	}
+
+	return categorizedItems, nil
+}
